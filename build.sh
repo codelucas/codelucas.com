2434440ab05d6980707d8fbb3fa3c2f7333d00c1
codelucas <lucasyangpersonal@gmail.com>
2013-11-10 08:29:39 +0000

    changed git css, modified build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ jekyll build
 # Run this compile if we are publishing, css is compressed
 if [ "$1" = "publish" ]
 then
-    rm ~/codelucas_dev/css/compiled/*
+    rm ~/codelucas_dev/css/compiled/*.css
     compass compile --output-style compressed
     echo "css has been compiled and compressed"
 else
@@ -16,5 +16,5 @@ else
 fi
 
 # Actually copy if over due to weird webfaction structure
-cp -rf css ~/webapps/codelucas
+cp -r css ~/webapps/codelucas
 
