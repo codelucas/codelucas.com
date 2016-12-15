a0a041de1cd351418ca2cf396dc5418c3a894319
codelucas <lucasyangpersonal@gmail.com>
2016-12-14 16:59:08 -0800

    Huge changes to personal site, revampted theme (colors, photos, backgrounds), changed bios/resumes (per my job switch), layout changes, more

--- art/index.md
+++
@@ -1,8 +0,0 @@
----
-layout: layout
-title: Art
----
-
-![Purple church](/images/india.jpg)
-
-**More to come soon...**
--- js/script_1.js
+++ js/script_1.js
@@ -48,6 +48,22 @@ $(document).ready(function () {
 
 $(window).load(function () {
 
+  $("a[href^='http://']").each( 
+    function(){ 
+     if(this.href.indexOf(location.hostname) == -1) { 
+        $(this).attr('target', '_blank'); 
+      } 
+    } 
+  );
+
+  $("a[href^='https://']").each( 
+    function(){ 
+      if(this.href.indexOf(location.hostname) == -1) { 
+        $(this).attr('target', '_blank'); 
+      } 
+    } 
+  );
+  
   // Fade prev/next post links on scroll
   if ($('body').hasClass('post')) {
 
